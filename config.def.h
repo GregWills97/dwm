232edacc433fef57fa752a124009572c96c38d32
Gregory Williams <gregwills85@gmail.com>
2023-04-06 14:11:28 -0600

    Add bar height in config.h

--- config.def.h
+++ config.def.h
@@ -2,10 +2,11 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const int startwithgaps	    = 0;	    /* 1 means gaps are used by default */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const int startwithgaps	    = 1;	    /* 1 means gaps are used by default */
 static const unsigned int gappx     = 10;       /* default gap between windows in pixels */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int barheight          = 2;        /* bar height */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int startwithbargaps   = 1;		/* 0 means no gaps at start */
@@ -27,7 +28,7 @@ static const char *colors[][3]      = {
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
-static const unsigned int baralpha    = 0xFF;
+static const unsigned int baralpha    = 0xDF;
 static const unsigned int borderalpha = OPAQUE;
 static unsigned int alphas[][3]      = {
 	/*               fg         bg         border   */
