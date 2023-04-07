9552d1828f747999428f956183651c6819dff582
Gregory Williams <gregwills85@gmail.com>
2023-04-06 22:16:05 -0600

    Have chosen colorscheme

--- config.def.h
+++ config.def.h
@@ -2,11 +2,11 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const int startwithgaps	    = 1;	    /* 1 means gaps are used by default */
 static const unsigned int gappx     = 10;       /* default gap between windows in pixels */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int barheight          = 2;        /* bar height */
+static const int barheight          = 6;        /* bar height */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int startwithbargaps   = 1;		/* 0 means no gaps at start */
@@ -22,11 +22,13 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+//static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#47407D";
+static const char col_border[]      = "#569fba";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_border },
 };
 static const unsigned int baralpha    = 0xDF;
 static const unsigned int borderalpha = OPAQUE;
@@ -38,7 +40,6 @@ static unsigned int alphas[][3]      = {
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-//static const char *tags[] = { "", "", "", "", "", "", "", "", ""};
 static const char *tags[] = { "", "", "", "", "", "", ""};
 
 static const Rule rules[] = {
