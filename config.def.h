3dfe4016a0f81c7769b615ff487707da08d980e8
Gregory Williams <gregwills85@gmail.com>
2023-04-08 20:17:37 -0600

    Added highlight colors to dmenu command

--- config.def.h
+++ config.def.h
@@ -17,18 +17,17 @@ static const char *fonts[]          = { "Liberation Mono:style=Regular:size=10:a
 										"Font Awesome 5 Free Regular:style=Regular:size=10:antialias=true:autohint=true",
 										"Font Awesome 5 Free Solid:style=Solid:size=10:antialias=true:autohint=true",
 										"Font Awesome 5 Brands Regular:style=Regular:size=10:antialias=true:autohint=true" };
-static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 //static const char col_cyan[]        = "#005577";
 static const char col_cyan[]        = "#47407D";
-static const char col_border[]      = "#569fba";
+static const char col_highlight[]   = "#569fba";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_border },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_highlight },
 };
 static const unsigned int baralpha    = 0xDF;
 static const unsigned int borderalpha = OPAQUE;
@@ -82,9 +81,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-c", "-l", "15",
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-l", "15",
 								  "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan,
-								  "-sf", col_gray4, "-p", "Run: ", NULL };
+								  "-sf", col_gray4, "-nhb", col_gray1, "-nhf", col_highlight,
+								  "-shb", col_cyan, "-shf", col_highlight, "-p", "Run: ", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
